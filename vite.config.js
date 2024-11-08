107ca676b6237f87d95764e39cdc271eee7355a2
Guoliang Cao <gcao99@gmail.com>
2024-11-08 11:57:18 -0500

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig, normalizePath } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteStaticCopy } from "vite-plugin-static-copy";
-import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,27 +9,11 @@ export default defineConfig({
     viteStaticCopy({
       targets: [
         {
-          src: normalizePath(
-            path.resolve(__dirname, "./node_modules/onnxruntime-web/dist") +
-              "/*.wasm"
-          ),
+          src: "node_modules/onnxruntime-web/dist/*.wasm",
           dest: ".",
         },
       ],
     }),
-    {
-      name: "configure-response-headers",
-      configureServer: (server) => {
-        server.middlewares.use((_req, res, next) => {
-          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-          next();
-        });
-      },
-    },
   ],
-  build: {
-    chunkSizeWarningLimit: 2000, // handle warning on vendor.js bundle size
-  },
-  base: "/rock-paper-scissors/",
+  base: "./",
 });
